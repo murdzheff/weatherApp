7073b5ddb32534fc16d77da86830b81e660d7b83
murdzheff <murdzheff@tuta.io>
2023-03-08 15:12:03 +0200

    adding js code

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-something
+let request = new XMLHttpRequest();
+request.open("GET", "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sofia?unitGroup=metric&key=9DK2NJ2HW36DZVYKJGVM364WC&contentType=json");
+request.send();
+request.responseType = "json";
+
+console.log(request);
