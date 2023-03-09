70302cfce451e75b81d656670a85345171f3f50b
murdzheff <murdzheff@tuta.io>
2023-03-09 17:33:15 +0200

    Added search bar, so you can now check weather by location

--- main.js
+++ main.js
@@ -5,78 +5,105 @@ let feelsLike = document.getElementById("feelsLike");
 let windSpeed = document.getElementById("windSpeed");
 let hum = document.getElementById("hum");
 let bg = document.getElementById("background");
-
 let grid = document.getElementById("forecastGrid");
-
 let request = new XMLHttpRequest();
-request.open("GET", "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sofia?unitGroup=metric&key=9DK2NJ2HW36DZVYKJGVM364WC&contentType=json");
-request.send();
-request.responseType = "json";
-request.onload = () => {
-    console.log(request.response)
-    address.innerText = request.response.resolvedAddress;
-    conditions.innerText = request.response.currentConditions.conditions;
-    temp.innerText = `Temp: ${request.response.currentConditions.temp} °C`;
-    feelsLike.innerText = `Feels like: ${request.response.currentConditions.feelslike} °C`;
-    windSpeed.innerText = `Wind speed is: ${request.response.currentConditions.windspeed} km/h ||`;
-    hum.innerText = `Humidity: ${request.response.currentConditions.humidity} %`
+let api = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/lesichovo?unitGroup=metric&key=9DK2NJ2HW36DZVYKJGVM364WC&contentType=json";
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
 
-    //tests for background
-
-    // conditions.innerText = "neshto si";
-    // conditions.innerText = "Rain, Overcast";
-    //conditions.innerText = "Partially cloudy";
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(api);
+    api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value.toString()}?unitGroup=metric&key=9DK2NJ2HW36DZVYKJGVM364WC&contentType=json`;
+    load();
+})
 
 
-    if (conditions.innerText === "Partially cloudy") {
-        bg.src = "./assets/weatherbg/clouds-16495.mp4";
-    } else if (conditions.innerText === "Rain, Overcast") {
-        bg.src = "./assets/weatherbg/rain.mp4";
-    } else {
-        bg.src = "./assets/weatherbg/sunny.mp4";
-    }
 
-    
 
-    request.response.days.forEach(element => {
-        let dayCard = document.createElement("div");
-        dayCard.id = "dayCard";
 
-        let date = document.createElement("h4");
-        date.innerText = element.datetime
+function load() {
+    request.open("GET", api);
+    request.send();
+    request.responseType = "json";
 
-        let img = document.createElement("img"); 
 
-        if (element.conditions === "Partially cloudy") {
-            img.src = "./assets/images/cloudy.png";
+
+
+    request.onload = () => {
+        console.log(request.response)
+        address.innerText = request.response.resolvedAddress;
+        conditions.innerText = request.response.currentConditions.conditions;
+        temp.innerText = `Temp: ${request.response.currentConditions.temp} °C`;
+        feelsLike.innerText = `Feels like: ${request.response.currentConditions.feelslike} °C`;
+        windSpeed.innerText = `Wind speed is: ${request.response.currentConditions.windspeed} km/h ||`;
+        hum.innerText = `Humidity: ${request.response.currentConditions.humidity} %`
+        grid.innerHTML = "";
+        //tests for background
+
+        // conditions.innerText = "neshto si";
+        // conditions.innerText = "Rain, Overcast";
+        //conditions.innerText = "Partially cloudy";
+
+
+        if (conditions.innerText === "Partially cloudy") {
+            bg.src = "./assets/weatherbg/clouds-16495.mp4";
+        } else if (conditions.innerText === "Rain, Overcast") {
+            bg.src = "./assets/weatherbg/rain.mp4";
         } else {
-            img.src = "./assets/images/rain.png";
+            bg.src = "./assets/weatherbg/sunny.mp4";
         }
 
-        let tempContainer = document.createElement("span");
-        let maxTemp = document.createElement("h5");
-        let minTemp = document.createElement("h5");
 
-        maxTemp.innerText = `Max: ${element.tempmax} °C`;
-        minTemp.innerText = `Min: ${element.tempmin} °C`;
+        request.response.days.forEach(element => {
+            let dayCard = document.createElement("div");
+            dayCard.id = "dayCard";
 
-        tempContainer.append(maxTemp,minTemp);
+            let date = document.createElement("h4");
+            date.innerText = element.datetime
 
-        dayCard.append(date,img,tempContainer);
+            let img = document.createElement("img");
 
-        grid.appendChild(dayCard);
+            if (element.conditions === "Partially cloudy") {
+                img.src = "./assets/images/cloudy.png";
+            } else {
+                img.src = "./assets/images/rain.png";
+            }
 
-        
-        
+            let tempContainer = document.createElement("span");
+            let maxTemp = document.createElement("h5");
+            let minTemp = document.createElement("h5");
 
-        
+            maxTemp.innerText = `Max: ${element.tempmax} °C`;
+            minTemp.innerText = `Min: ${element.tempmin} °C`;
+
+            tempContainer.append(maxTemp, minTemp);
+
+            dayCard.append(date, img, tempContainer);
+
+            grid.appendChild(dayCard);
 
 
-        
-    });
+
+
+
+
+
+
+        });
+
+
+    }
+
+
+
 
 
 }
+
+load();
+
+
 
 
 
